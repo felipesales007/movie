49812d1ef0d9256ebe8b68d3e100a7701a2daf7b
Felipe Sales <felipesales007@hotmail.com>
2022-02-26 03:16:25 -0300

    adicionando moment e env

---
+++ env.example.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'TMDB',
+  domain: 'https://api.themoviedb.org/3',
+  key: {
+    params: {
+      api_key: '361f6506acd5c4f91fad6237a00776eb'
+    }
+  },
+}
--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from './services/axios'
+import env from '../env'
+import moment from 'moment'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Components from './components'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/css/edit.css'
+import './assets/css/style.css'
+
+moment.locale('pt-br')
 
 Vue.use(axios)
 Vue.use(BootstrapVue)
@@ -16,6 +20,8 @@ Vue.use(IconsPlugin)
 Vue.use(Components)
 
 Vue.config.productionTip = false
+Vue.prototype.$env = env
+Vue.prototype.$moment = moment
 
 new Vue({
   router,
